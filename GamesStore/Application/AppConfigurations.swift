d9e10015c765ff6ad4fd68c7bcee6a3ba73e90f5
Ahmed shafik <a.mahmuod@gmail.com>
2023-02-15 21:19:48 +0200

    adding api key and base url to plist

---
+++ GamesStore/Application/AppConfigurations.swift
@@ -0,0 +1,23 @@
+//
+//  AppConfigurations.swift
+//  GamesStore
+//
+//  Created by Ahmed Shafik on 14/02/2023.
+//
+
+import Foundation
+
+final class AppConfiguration {
+    lazy var apiKey: String = {
+        guard let apiKey = Bundle.main.object(forInfoDictionaryKey: "ApiKey") as? String else {
+            fatalError("ApiKey must not be empty in plist")
+        }
+        return apiKey
+    }()
+    lazy var apiBaseURL: String = {
+        guard let apiBaseURL = Bundle.main.object(forInfoDictionaryKey: "ApiBaseURL") as? String else {
+            fatalError("ApiBaseURL must not be empty in plist")
+        }
+        return apiBaseURL
+    }()
+}
